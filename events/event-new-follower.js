594104737644a552f1f3d8814a0965b999213955
Timmy Kokke <mental_c@hotmail.com>
2019-04-23 22:44:15 +0200

    disabled follow event for now.

--- events/event-new-follower.js
+++ events/event-new-follower.js
@@ -5,15 +5,15 @@ const folder = process.env.MUXY_FOLDER;
 
 module.exports =
     (twitchClient, target) => {
-        fs.watchFile(`${folder}\\${file}`, () => {
-            fs.readFile(`${folder}\\${file}`, 'utf8', (err, follower)=> {
-                if(!!err){
-                    console.log(err);
-                    return;
-                }
-                if(follower!==""){
-                    twitchClient.say(target, `Hi @${follower}! Welcome to the coder-sphere.`);
-                }
-            })           
-        })            
+        // fs.watchFile(`${folder}\\${file}`, () => {
+        //     fs.readFile(`${folder}\\${file}`, 'utf8', (err, follower)=> {
+        //         if(!!err){
+        //             console.log(err);
+        //             return;
+        //         }
+        //         if(follower!==""){
+        //             twitchClient.say(target, `Hi @${follower}! Welcome to the coder-sphere.`);
+        //         }
+        //     })           
+        // })            
     }
