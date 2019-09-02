7e1a29dce84bc1eddc3a685be4f400a56909f909
Timmy Kokke <mental_c@hotmail.com>
2019-09-02 21:35:05 +0200

    updated todo + type

--- events/event-timedMessages.js
+++ events/event-timedMessages.js
@@ -7,7 +7,7 @@ const allMessages = [
     { msg: "All the VODs and more can be viewed at Sorskoot's YouTube channel. http://youtube.com/user/Sorskoot?sub_confirmation=1" },
     { msg: "Did you know Sorskoot also has a website! Go check it out, https://timmykokke.com. Well after the steam of course! " },
     { msg: "Feel free to ask Sorskoot questions, he's glad to help... " },
-    { msg: "Want to know what happend during the steam? Sorskoot is keeping notes and shares them at https://timmykokke.com/twitch/" },
+    { msg: "Want to know what happend during the stream? Sorskoot is keeping notes and shares them at https://timmykokke.com/twitch/" },
     { msg: "There are over 30 sound effects you can trigger in chat! For example !badumts or !justdoit. The whole list is at https://github.com/sorskoot/RosieBot/blob/master/commands.md#sound-fx" },
     { msg: "See that light behind Sorkoot? You can control that with !light" },
     { msg: "If you are enjoing the stream don't forget to hit the follow button ;)" },
