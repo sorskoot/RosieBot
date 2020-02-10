bcb43e5f2eeb576b55c3b907f9e8fe8c6682b37a
Timmy Kokke <mental_c@hotmail.com>
2020-02-10 22:33:48 +0100

    added the social links overlay

---
+++ public/overlays/social/social.js
@@ -0,0 +1,12 @@
+(function () {
+    let socials = document.querySelectorAll(".social");
+    let currentSocial = 0;
+    setInterval(() => {
+        socials[currentSocial].classList.add("active");
+        setTimeout(() => {
+            socials[currentSocial].classList.remove("active");
+            currentSocial = (currentSocial + 1) % socials.length;
+        }, 10000);
+    }, 12000)
+
+})();
