66fb613e985b481d4509da0544d2ba38ab69a12a
Timmy Kokke <mental_c@hotmail.com>
2020-03-13 16:43:44 +0100

    fixed latest issue in YouTube Remote.

--- src/plugins/actions/youtube-remote.action.js
+++ src/plugins/actions/youtube-remote.action.js
@@ -22,16 +22,13 @@ class YouTubeRemoteAction extends Action {
             let r = new RegExp(myregexp);
             let result = r.exec(params[0].message);
             
-
+            await this.$store.dispatch("socket/emit", {event: "player",args: "pause"});
             await this.$store.dispatch('triggerAction/trigger',{
                 uuid:'rosie.core.trigger.immidiate', 
                     eventName:'com.sorskoot.midi.action',
-                    params: [{"message": [177,7,40]}]
+                    params: [{"message": [177,7,80]}]
                 })
-                this.$store.dispatch("socket/emit", {
-                    event: "player",
-                    args: "pause"
-                  });
+            
             await this.$store.dispatch("socket/emit", {
                 event: "youtube-player",
                 args: { play: result[1] }
